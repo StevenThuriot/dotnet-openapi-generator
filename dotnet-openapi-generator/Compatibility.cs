1cf154bc84ee9ffcd78497788d5d2fb4265eea2c
Steven Thuriot <steven@thuriot.be>
2023-11-02 20:44:38 +0100

    Added Compatibility for older dotnet versions

---
+++ dotnet-openapi-generator/Compatibility.cs
@@ -0,0 +1,18 @@
+﻿namespace dotnet.openapi.generator;
+internal static class Compatibility
+{
+#if !NET6_0_OR_GREATER
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+    {
+        HashSet<TKey> set = new();
+
+        foreach (TSource item in source)
+        {
+            if (set.Add(keySelector(item)))
+            {
+                yield return item;
+            }
+        }
+    }
+#endif
+}
